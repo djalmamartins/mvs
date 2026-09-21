65b3f57a1da57cdf4aed0705f84c93901a477e14
Djalma Martins <djalma-martins@hotmail.com>
2026-09-20 21:19:42 -0300

    feat: register Talk queue claim route

--- app/Boot/Routes.php
+++ app/Boot/Routes.php
@@ -116,6 +116,7 @@ final class Routes
 
         $router->get('/talk', 'TalkController:dashboard', 'talk.dashboard', AuthMiddleware::class);
         $router->get('/talk/queue', 'TalkController:queue', 'talk.queue', AuthMiddleware::class);
+        $router->post('/talk/queue', 'TalkController:queue', 'talk.queue.claim', AuthMiddleware::class);
         $router->get('/talk/conversations', 'TalkController:conversations', 'talk.conversations', AuthMiddleware::class);
         $router->get('/talk/contacts', 'TalkController:contacts', 'talk.contacts', AuthMiddleware::class);
         $router->get('/talk/my-tickets', 'TalkController:myTickets', 'talk.my-tickets', AuthMiddleware::class);
