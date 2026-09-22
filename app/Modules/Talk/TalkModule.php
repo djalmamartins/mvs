fea97d4d3d7e3d2c0e80cc9ca9ec83d895476c01
Djalma Martins <djalma-martins@hotmail.com>
2026-09-21 22:11:01 -0300

    feat(talk): registrar módulo e rotas protegidas (#108)

---
+++ app/Modules/Talk/TalkModule.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Moves\Modules\Talk;
+
+use Moves\Boot\Modules;
+use Moves\Middleware\AuthMiddleware;
+use Moves\Middleware\PermissionMiddleware;
+use MovesCode\Router\Router;
+
+final class TalkModule
+{
+    public static function register(): void
+    {
+        Modules::register(
+            'talk',
+            static function (Router $router): void {
+                $middleware = [AuthMiddleware::class, new PermissionMiddleware('talk.access')];
+                $router->get('/talk', 'TalkController:index', 'talk.home', $middleware);
+                $router->get('/talk/queue', 'TalkController:index', 'talk.queue', $middleware);
+            },
+            ['admin' => ['talk.access']]
+        );
+    }
+}
