767f69090b61fe25b8080c81950b7fbbe2b1b7f3
Djalma Martins <djalma-martins@hotmail.com>
2026-09-22 07:30:10 -0300

    test(talk): validar contrato seguro do webhook WhatsApp (#97)

---
+++ tests/TalkWhatsAppWebhookContractTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class TalkWhatsAppWebhookContractTest extends TestCase
+{
+    public function testWebhookRequiresVerificationAndSignedPayload(): void
+    {
+        $source=(string)file_get_contents(__DIR__.'/../app/Controllers/TalkWebhookController.php');
+        self::assertStringContainsString('TALK_WHATSAPP_VERIFY_TOKEN',$source);
+        self::assertStringContainsString('TALK_WHATSAPP_APP_SECRET',$source);
+        self::assertStringContainsString('HTTP_X_HUB_SIGNATURE_256',$source);
+        self::assertStringContainsString("hash_hmac('sha256'",$source);
+        self::assertStringContainsString("phone_number_id",$source);
+        self::assertStringContainsString("external_account_id=:external_id",$source);
+    }
+}
